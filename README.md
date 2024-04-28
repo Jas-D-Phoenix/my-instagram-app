42b3f6c30bda72d1986a62d118e94cf5bda002c8
Miriam Musa <108754509+Jas-D-Phoenix@users.noreply.github.com>
2024-04-28 10:11:50 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Welcome to My Instagram
+***
+
+## Task
+I am to create a clone of instagram using react and aws.
+
+## Description
+I created an instagram clone and hosted it on a cloud. The clone has a lot of features found on instagram.
+
+## Installation
+You click on the link below and access the site.
+
+## Usage
+
+by clicking https://647f0e503c60030d6b9f266c--gentle-madeleine-a5b691.netlify.app
+you sign up and create an account.
+
+### The Core Team
+it was created by Miriam Musa.
+
+<span><i>Made at <a href='https://qwasar.io'>Qwasar SV -- Software Engineering School</a></i></span>
+<span><img alt='Qwasar SV -- Software Engineering School's Logo' src='https://storage.googleapis.com/qwasar-public/qwasar-logo_50x50.png' width='20px'></span>
---
+++ src/constants/RandomUsers.js
@@ -0,0 +1,44 @@
+export const FakeUsers = [
+
+  {
+    photoURL:
+      "https://st2.depositphotos.com/1003697/10774/i/450/depositphotos_107745282-stock-photo-young-business-man-copy-documents.jpg",
+    username: "Billy Hardy",
+    name: "Billy Hardy",
+  },
+
+  {
+    photoURL:
+      "https://i.pinimg.com/736x/e9/98/69/e998694ee41d1986778dbdd2efcd18b4.jpg",
+    username: "henry_cavil",
+    name: "Henry Cavil",
+  },
+  {
+    photoURL:
+      "https://static.vecteezy.com/system/resources/thumbnails/003/492/044/small/attractive-young-woman-pointing-by-her-finger-sideways-place-for-copy-space-free-photo.jpg",
+    username: "Veron Philip",
+    name: "Veron Philip",
+  },
+  {
+    photoURL:"https://img.freepik.com/free-photo/side-view-athletic-man-with-rope-copy-space_23-2148398877.jpg",
+    username: "Fredie Shell",
+    name: "Fredie Shell",
+  },
+  {
+    photoURL: "https://img.freepik.com/free-photo/portrait-young-man-pointing-fingers-copy-space-isolated-white-studio-background_1150-63428.jpg?",
+    username: "Jamie Lad",
+    name: "Jamie Lad",
+  },
+  {
+    photoURL:
+      "https://tty-art.com/file/2020/01/Generated-Faces-by-AI-Teens-Girls-V1-001-1.jpg",
+    username: "Bridget Bale",
+    name: "Bridget Bale",
+  },
+  {
+    photoURL:
+      "https://st4.depositphotos.com/1002314/20858/i/600/depositphotos_208583182-stock-photo-young-student-copy-center-taking.jpg",
+    username: "Sarah Bond",
+    name: "Sarah B",
+  }
+];
